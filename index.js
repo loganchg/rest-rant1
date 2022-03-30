50af735f7836c6b443dc0d774fc4e4202adf06b7
Logan <L@MacBook-Pro.local>
2022-03-30 14:01:46 -0700

    new changes

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places', require('./controllers/places))
-
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
