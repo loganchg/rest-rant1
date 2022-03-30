18d60274779f9fc9b908644e5816be3d83a27f55
Logan <L@MacBook-Pro.local>
2022-03-30 16:39:18 -0700

    Adding styles to REST-Rant

---
+++ views/places/index.jsx
@@ -0,0 +1,36 @@
+const React = require('react');
+const Default = require('../default');
+
+function Index (data) {
+    let placesFormatted = data.places.map((place, index) => {
+        return (
+            <div className="col-sm-6">
+                <h2>
+                    <a href={`/places/${index}`}>
+                        {place.name}
+                    </a>
+                </h2>
+                <p className="text-left">
+                    {place.cuisines}
+                </p>
+                <img className="img-fluid" src={place.pic} alt={place.name}/>
+                <p className="text-left">
+                    Located in {place.city}, {place.state}
+                </p>
+            </div>
+        );
+    });
+
+    return (
+        <Default>
+            <main>
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                    {placesFormatted}
+                </div>
+            </main>
+        </Default>
+    );
+};
+
+module.exports = Index;
