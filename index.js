9819868db02ceab111fcf385abb22ccc61eb1f61
Logan <L@MacBook-Pro.local>
2022-03-30 17:57:37 -0700

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
+// Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
-  })
+})
 
 app.get('*', (req, res) => {
     res.render('error404')
-  })
+})
 
-// GET /places
-app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-    res.render('places/index')
-  })
-
+// Listen for Connections
 app.listen(process.env.PORT)
-
--- views/error404.jsx
+++ views/error404.jsx
@@ -1,14 +1,21 @@
-const React = require('react')
-const Def = require('./default')
+const React = require('react');
+const Default = require('./default')
 
 function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-          </main>
-      </Def>
-    )
-  }
-module.exports = error404
+    return( 
+        <Default>
+            <main>
+                <h1>404: PAGE NOT FOUND</h1>
+                <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                <img src='http://placekitten.com/250/250' alt="Chia Fruit Shake" style={{width: 800}}/>
+                <div>
+                    Photo by <a href="https://flickr.com/photos/bigtallguy/">BigTallGuy</a> license: <a href="https://creativecommons.org/licenses/by/2.0/">license:</a> on <a href="placekitten.com/250/250">Place Kitten</a>
+                </div>
+            </div>
+            </main>
+        </Default>
+    );
+};
+
+module.exports = error404;
