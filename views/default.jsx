4c2e279be769e7e690d2e6e1e181ba6ae15c80c5
Logan <L@MacBook-Pro.local>
2022-04-05 23:17:03 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,17 @@
-const React = require('react');
+const React = require('react')
 
-function Default (html) {
+function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
-                <link 
-                    rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-                    crossorigin="anonymous"
-                />     
+                <title>RestRant Live Demo</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
-                </head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest"></link>
+            </head>
             <body>
                 <nav>
                     <ul>
@@ -23,14 +22,14 @@ function Default (html) {
                             <a href="/places">Places</a>
                         </li>
                         <li>
-                            <a href="/places/new">Add Place</a>
+                            <a href="/places/new">New Place</a>
                         </li>
                     </ul>
                 </nav>
                 {html.children}
             </body>
         </html>
-    );
-};
+    )
+}
 
-module.exports = Default;
+module.exports = Def
