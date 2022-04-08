cea0b925e16e13e0476b7601920636fe6f5559a0
Logan <L@MacBook-Pro.local>
2022-04-07 23:28:09 -0700

    Added the edit form

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,5 +1,15 @@
-const React = require('react');
-const Default = require('../default');
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const methodOverride = require('method-override')
+
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 function Index (data) {
     let placesFormatted = data.places.map((place, index) => {
